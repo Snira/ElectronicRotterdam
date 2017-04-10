95e4ede5c5e12230c878048b9d48cb15007e61ee
Snira <gbhavelaar@gmail.com>
2017-04-10 14:40:43 +0200

    skrt

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,8 @@
 <div class="container">
     <h1 class="h1 text-center">Title</h1>
         <div class="search">
-            <input id="search" class="center-block" type="search">
+            <label for="search"></label>
+            <input id="search" class="center-block" type="search" placeholder="Search your city">
         </div>
 
 </div>
